23e2042e95a027ae362ab07c5a85f9e0f457e2a8
saurumane <saurumane7032@gmail.com>
2023-11-27 12:37:27 +0530

    this commit will be including all the lambda code for processing the file from s3 bucket

---
+++ fourthlambda.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+import csv
+
+
+def lambda_handler(event, context):
+    s3_client = boto3.client('s3')
+    bucket = 'pmsprojecttesting'
+    input_key = 'input/saurabh.csv'
+    output_key = 'output/saurabh_processed30_to_40.csv'
+
+    input_file_path = '/tmp/saurabh.csv'
+    output_file_path = '/tmp/saurabh_processed30_to_40.csv'
+
+    # Download the input file from S3
+    s3_client.download_file(bucket, input_key, input_file_path)
+
+    # Process and write rows 30 to 40 to a new file
+    new_rows = []
+    with open(input_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for index, row in enumerate(csv_reader, start=1):
+            if 32 <= index <= 41:
+                new_rows.append(row)
+
+    # Write the selected rows to a new file
+    with open(output_file_path, 'w', newline='') as new_csv_file:
+        csv_writer = csv.writer(new_csv_file)
+        csv_writer.writerows(new_rows)
+
+    # Upload the processed file to the output folder in S3
+    s3_client.upload_file(output_file_path, bucket, output_key)
+
+    response = {
+        "statusCode": 200,
+        "body": "\"Processed rows 30 to 40 and saved to output folder\"",
+        "success": True
+    }
+    return response
